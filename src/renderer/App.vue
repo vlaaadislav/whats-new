26a8542e7994481918c12349c97cfbea64002a77
Vlad <vlaaadislav@gmail.com>
2018-04-16 22:58:24 +0500

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'whats-new'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
